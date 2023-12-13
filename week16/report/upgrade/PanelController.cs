0f3881ab1199b648acaea73582678c9bbce01ca3
27sh <120109008+27sh@users.noreply.github.com>
2023-12-13 14:50:53 +0900

    Create PanelController.cs

---
+++ week16/report/upgrade/PanelController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PanelController : MonoBehaviour
+{
+    public void OpenPanel()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void ClosePanel()
+    {
+        gameObject.SetActive(false);
+    }
+}
